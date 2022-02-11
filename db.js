7585dc4c0858aaacaba3c57d0ac24c2bd7743af5
Kenneth <KennethWCochran1010@gmail.com>
2022-02-11 12:57:50 -0500

    quick change

--- db.js
+++ db.js
@@ -2,6 +2,9 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://${process.env.DATABASE_USERNAME}:${encodeURIComponent(process.env.DATABASE_PASSWORD)}@localhost/${process.env.DATABASE_NAME}`, {
     dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
+    dialectOptions: {
+        ssl: process.env.ENVIRONMENT === 'production'
+    }
+
 })
 module.exports = sequelize;
